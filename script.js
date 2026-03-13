f01b075f9a3b698d281e5f0f856d907b1e398df9
Dorach Records <dorachrecords@gmail.com>
2026-03-13 09:08:35 -0500

    Crear script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+/* SCROLL SUAVE */
+
+const links = document.querySelectorAll("nav a");
+
+links.forEach(link => {
+
+link.addEventListener("click", e => {
+
+e.preventDefault();
+
+const target = document.querySelector(
+link.getAttribute("href")
+);
+
+target.scrollIntoView({
+
+behavior:"smooth"
+
+});
+
+});
+
+});
